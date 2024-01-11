a7193188c3e5e28b11bf04d9c6e001153ed4c4c9
DarkCodes777 <39138636+darkcodes777@users.noreply.github.com>
2024-01-11 22:01:08 +0100

    modifiche struttura cartelle

--- ACC_V401_ZON/MainWindow.xaml.cs
+++ ACC_V401_ZON/MainWindow.xaml.cs
@@ -18,6 +18,8 @@ namespace ACC_V401_ZON
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+    
     public partial class MainWindow : Window
     {
         public MainWindow()
